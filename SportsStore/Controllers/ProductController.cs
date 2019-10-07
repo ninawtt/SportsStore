de4b469ae1d16210f0df58ea2871d55fc58c5490
Nina <ninawu424@gmail.com>
2019-10-07 15:54:26 -0400

    Added and formated pages to show the product information

--- SportsStore/Controllers/ProductController.cs
+++ SportsStore/Controllers/ProductController.cs
@@ -4,12 +4,14 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using SportsStore.Models;
+using SportsStore.Models.ViewModels;
 
 namespace SportsStore.Controllers
 {
     public class ProductController : Controller
     {
         private IProductRepository repository;
+        public int PageSize = 4;
 
         // Constructor
         public ProductController(IProductRepository repo)
@@ -18,6 +20,19 @@ namespace SportsStore.Controllers
         }
 
         // default action
-        public ViewResult List() => View(repository.Products);
+        public ViewResult List(int productPage = 1) => 
+            View( new ProductsListViewModel
+            {
+                Products = repository.Products
+                    .OrderBy(p => p.ProductID)
+                    .Skip((productPage - 1) * PageSize)
+                    .Take(PageSize),
+                PagingInfo = new PagingInfo
+                {
+                    CurrentPage = productPage,
+                    ItemsPerPage = PageSize,
+                    TotalItems = repository.Products.Count()
+                }
+            });
     }
 }
