0c5a006ffaffcf27976ce3aedc1508b0163e2297
Nina <ninawu424@gmail.com>
2019-10-20 23:52:33 -0400

    Created the Order repository

---
+++ SportsStore/Models/EFOrderRepository.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SportsStore.Models
+{
+    public class EFOrderRepository : IOrderRepository
+    {
+        private ApplicationDbContext context;
+
+        // Constructor 
+        public EFOrderRepository(ApplicationDbContext ctx)
+        {
+            context = ctx;
+        }
+
+        public IQueryable<Order> Orders => context.Orders
+            .Include(o => o.Lines)
+                .ThenInclude(l => l.Product);
+
+        public void SaveOrder(Order order)
+        {
+            // Avoid the application to insert products into Product table again
+            // Avoid duplicate
+            context.AttachRange(order.Lines.Select(l => l.Product));
+            if (order.OrderID == 0)
+            {
+                context.Orders.Add(order);
+            }
+            context.SaveChanges();
+        }
+    }
+}
--- SportsStore/Startup.cs
+++ SportsStore/Startup.cs
@@ -34,6 +34,8 @@ namespace SportsStore
             services.AddScoped<Cart>(sp => SessionCart.GetCart(sp));
             services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
 
+            services.AddTransient<IOrderRepository, EFOrderRepository>();
+
             services.AddMvc();
             services.AddMemoryCache();
             services.AddSession();
